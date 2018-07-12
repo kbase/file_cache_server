31da0b1c1bde7940c011512accf77aee2c13589e
Jay R Bolton <jayrbolton@gmail.com>
2018-07-11 20:09:53 -0700

    Add error handler for when the json body is empty when generating a cache id

--- caching_service/cache_id/generate_cache_id.py
+++ caching_service/cache_id/generate_cache_id.py
@@ -17,6 +17,8 @@ def generate_cache_id(token, json_data):
     if not token or not isinstance(token, str):
         raise TypeError('`token` must be a non-empty string')
     # Get a uniform json string with keys sorted and whitespace removed
+    if not json_data:
+        raise TypeError('Must provide non-empty json_data for the cache identifier')
     json_text = json.dumps(json_data, sort_keys=True)
     concatenated = token + '\n' + json_text
     return bhash(concatenated)
