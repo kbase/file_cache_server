c1bc1154dd5cff552507a8ce50470fafbc82d25c
Jay R Bolton <jayrbolton@gmail.com>
2021-02-16 14:55:50 -0800

    Only run bandit on source code, not tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,5 +5,5 @@ set -e
 flake8 --max-complexity 6 src/caching_service
 flake8 src/test
 mypy --ignore-missing-imports src
-bandit -r src
+bandit -r src/caching_service
 python -m unittest discover src/test/caching_service
