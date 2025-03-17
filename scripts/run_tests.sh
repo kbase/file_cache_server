9b4eeb9b7557ea4a8d188d81966d849e4a4e7b86
Sijie <sijiex@lbl.gov>
2025-03-17 13:47:54 -0700

    upload coverage.xml

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,4 +6,4 @@ flake8 --max-complexity 6 src/caching_service
 flake8 src/test
 mypy --ignore-missing-imports src
 bandit -r src/caching_service
-PYTHONPATH=. pytest -s -vv --cov=src/caching_service --cov-report term --cov-report html src/test/caching_service
+PYTHONPATH=. pytest -s -vv --cov=src/caching_service --cov-report=term --cov-report=xml src/test/caching_service
